730f37ffeffa478823ce4b051ca576b9724ae080
Tim Austin <tim@neenjaw.com>
2018-10-16 23:24:14 -0600

    formatting changes

--- user/User.js
+++ user/User.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');  
 var UserSchema = new mongoose.Schema({  
-  name: String,
-  email: String,
-  password: String
+    name: String,
+    email: String,
+    password: String
 });
 mongoose.model('User', UserSchema);
 
